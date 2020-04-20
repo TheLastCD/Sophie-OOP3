6ba0e159b8288693af1bd45527dc5e3bd886cba9
Charlie N A <lambopuggy@gmail.com>
2020-04-20 19:01:36 +0100

    fixed issues we had moved the location of the stored files into the constructor of the Readfiles class adds a bit of complexity for higher OOP Mark Added Overflow for longer sentences so it only shows 5 words after the difference

--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@ namespace Git_Diff
 {
     class Program
     {
+        //tester diff GitRepositories_2a.txt GitRepositories_2b.txt
         //notes:
         // UI needs rebuilding, might struggle with how the verifcation is arranged
         // Still need to figure out how to define - or +
@@ -17,8 +18,7 @@ namespace Git_Diff
         static void Main(string[] args)
         {
 
-            Console.WriteLine("The following documents are available for comparison: \n1 GitRepositories_1a \n2 GitRepositories_1b \n3 GitRepositories_2a \n4 GitRepositories_2b \n5 GitRepositories_3a \n6 GitRepositories_3b");
-            Console.WriteLine("Please select the first document you wish to compare: ");
+            Console.Write(">: [Input]  ");
 
             string[] Userinput = new string[3];
             bool Files_Same = true;
@@ -27,7 +27,7 @@ namespace Git_Diff
                 do
                 {
                     Userinput = Console.ReadLine().Split();
-                    if ((Userinput.Length != 2 || Userinput[0].ToLower() == "diff"))
+                    if ((Userinput.Length != 3 || Userinput[0].ToLower() != "diff"))
                     {
                         Console.WriteLine("There has been an error");
                     }
@@ -39,41 +39,38 @@ namespace Git_Diff
 
                 }
                 while (true);
-                string First_File = ReadFiles.readFiles(Userinput[1]);
-                StreamReader First_Reader = new StreamReader(Userinput[1]);
-                StreamReader Second_Reader = new StreamReader(Userinput[2]);
+                ReadFiles files = new ReadFiles(Userinput[1].ToString(), Userinput[2].ToString());
+                Files_Same = Compare_Files(files.First_File, files.Second_File);
 
             }
-            catch (Exception e)
+            catch(Exception e)
             {
                 Console.WriteLine($"{e.Message}");
             }
 
             // The bool from comparefiles method is returned and stored locally;
-
+            
             // If the bool is true, then the files are the same 
             if (Files_Same == true)
             {
                 Console.WriteLine(" The two files that you have selected are the same");
             }
-
-            //Otherwise they are different
             else
             {
                 Console.WriteLine("The compared that you have selected are not the same");
-                //C: new method here
+
             }
         }
 
         // Method for comparing the two text files as arguments
         static bool Compare_Files(string First_File, string Second_File)
         {
-            bool Files_Same;
-            Files_Same = true;
+            bool Files_Same = true;
             string[] FirstSentences = First_File.Split(".");
             string[] SecondSentences = Second_File.Split(".");
-
-            string Output = "";
+            int overflow = 1;
+            string Output ="";
+            Console.Write(">: Output ");
 
             for (int I = 0; I < FirstSentences.Length; I++)
             {
@@ -85,48 +82,63 @@ namespace Git_Diff
 
                     for (int R = 0; R < FirstSentenceWord.Length; R++)
                     {
+                        if (overflow == 0)
+                            break;
                         if (FirstSentenceWord[R] != SecondSentenceWord[R])
                         {
-                            if (FirstSentenceWord[I] != SecondSentenceWord[R + 1])
+                            if (FirstSentenceWord[I] != SecondSentenceWord[R+1])
                             {
                                 Output += SecondSentenceWord[R];
                                 Console.ForegroundColor = ConsoleColor.Green;
-                                Console.Write($"{SecondSentenceWord[R]}");
+                                Console.Write($"{SecondSentenceWord[R]} ");
                             }
                             else
                             {
                                 Output += SecondSentenceWord[R];
                                 Console.ForegroundColor = ConsoleColor.Red;
-                                Console.Write($"{FirstSentenceWord[R]}");
+                                Console.Write($"{FirstSentenceWord[R]} ");
                             }
 
                             Console.ForegroundColor = ConsoleColor.White;
+                            overflow = -5;
                         }
 
                         else
                         {
                             Output += SecondSentenceWord[R];
-                            Console.Write($"{FirstSentenceWord[R]}");
-
+                            Console.Write($"{FirstSentenceWord[R]} ");
+                           
                         }
+                        overflow++;
                         logger($"sentence: {I}\n{Output}");
                     }
-
                 }
 
             }
             return Files_Same;
         }
 
-        static void logger(string args)
+       static void logger(string args)
         {
             File.WriteAllText(@"logfile.txt", args);
         }
     }
 
-    // This class reads a singular file
+    // This class holds the two files as strings
+    // its also responsible for reading the files
+
     class ReadFiles
     {
-        return File.ReadAllLines(File_Choice).ToString();
+        public string First_File, Second_File;
+        public ReadFiles(string f, string s)
+        {
+            First_File = readFiles(f);
+            Second_File = readFiles(s);
+        }
+        // The method of the class
+        private string readFiles(string File_Choice)
+        {
+            return File.ReadAllText(File_Choice).ToString();
+        }
     }
 }
